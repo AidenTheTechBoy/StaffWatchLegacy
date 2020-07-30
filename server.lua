e19348fbe1b18d2c2da8d6fd6f0ea0cf3557d855
Aiden <43355457+AidenTheTechBoy@users.noreply.github.com>
2020-07-30 12:24:53 -0400

    Update server.lua

--- server.lua
+++ server.lua
@@ -3,7 +3,7 @@
 -------------------------------------------------------------------------------------------
 -- In-Game Message (For Advanced Users)
 function sendMessage(message, scope)
-    TriggerClientEvent('chat:addMessage', -1, {        ----------------------------------------------------------------------------
+    TriggerClientEvent('chat:addMessage', scope, {        ----------------------------------------------------------------------------
            color = { 255, 0, 0},                      --- Have a custom chat plugin? Want to change the text format of messages?
            multiline = true,                          --- Edit the TriggerClientEvent() function so it fits your needs!
            args = {"[StaffWatch] "..message}          ----------------------------------------------------------------------------
